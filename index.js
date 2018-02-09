29f5942fec4eda845e5082ba77b0170a97a78f45
Alexander Zhloba <AZhloba@VIACODE.COM>
2018-02-09 17:35:22 +0300

    initial version

---
+++ index.js
@@ -0,0 +1,27 @@
+
+function signIn() {
+
+    var host = $("#inputUrl").val();
+    $.ajax({
+        type: 'GET',
+        url: host,
+        async: true,
+        //data: JSON.stringify(d),
+        //dataType: "json",
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
+    })
+    .done(function (data) {        
+        showAlert(data);
+    })
+    .fail(function (error) {       
+        showAlert(error);
+    });
+};
+
+function showAlert(message) {
+    $("#alertContent").html(message);
+    $("#alert").alert();
+};
