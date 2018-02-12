8c3453529c78a677f6e601a6b61d65d5658fa6a6
Alexander Zhloba <AZhloba@VIACODE.COM>
2018-02-12 22:44:13 +0300

    changed to POST

--- index.js
+++ index.js
@@ -3,14 +3,24 @@ function signIn() {
 
     var host = $("#inputUrl").val();
     $.ajax({
-        type: 'GET',
+        type: 'POST',
         url: host,
         async: true,
         crossDomain: true,
         xhrFields: {
             withCredentials: true
         },        
-        dataType: "json"
+        dataType: "json",
+        data: JSON.stringify(
+            {
+                login: $("#inputEmail").val(),
+                password: $("#inputPassword").val()
+            }
+        ),
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
     })
     .done(function (data) {        
         showAlert(data);
